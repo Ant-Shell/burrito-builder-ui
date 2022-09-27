ba2bcba5bc8f9f6dddb1b8d6089d8d7a601250a1
Anthony Shellman <atshellman@gmail.com>
2022-09-27 11:30:54 -0600

    Add postOrders funciton, pass props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,12 +19,23 @@ class App extends Component {
     .catch(err => console.error('Error fetching:', err));
   }
 
+   postOrders = (name, ingredients) => {
+    return fetch('http://localhost:3001/api/v1/orders', {
+      method: 'POST',
+      body: JSON.stringify({name: name, ingredients: ingredients}),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(data => this.setState({orders: [...this.state.orders, data]}))
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm postOrders={this.postOrders} orders/>
         </header>
 
         <Orders orders={this.state.orders}/>
